1f697d0550afc546d857b610e5603694b278a92f
shu3bham <shubhamdeshmukhnagpur@gmail.com>
2023-05-20 17:31:23 +0530

    change folder directory and delete few thinks

---
+++ python100/old data/band_name_generator.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Generator...')
+Street = input('Enter the Name of City You Born in! ')
+Pet = input('Enter the Name of your Pet! ') 
+print('Your Band Name Could be '+ Streetfood+' '+Pet)
